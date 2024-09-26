c5c8b140708622a0b6d4257b7c6556a9ee065361
bjarte-johansen <bjartej@hotmail.com>
2024-09-26 17:49:21 +0200

    bjarte_endelig_all_tests_passed

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -1,52 +1,322 @@
 package no.hvl.dat100.matriser;
 
-public class Matriser {
+/*
+ * vi har definert to utility klasser MatrixUtils og VectorUtils for enkelhetsskyld
+ * vi har brukt StringBuilder istedenfor plain String i streng-metoder fordi det
+ * kan fungere bedre med minneallokering. 
+ */
 
+class MatrixUtils{
+	// fin måte å skrive ut matriser på som kan brukes til å debugge matrisefunksjoner
+	// en 2x2 matrise med numericStringWidth 6 blir seende slik ut
+	// {      1,     2 }
+	// {      3,     4 }
+	public static String toPrettyString(int[][] matrise, int numericStringWidth) {
+		// vi bruker StringBuilder. Den har append og allokerer
+		// minne automatisk. sBuilder.append (uttrykk) er litt som s += (uttrykk)
+		// men med et bedre interface og med færre kopier vanligvis
+		//
+		// vi bruker String.format med format "%" + numericStringWidth + "d"
+		// slik at ved å endre numericStringWidth kan vi tvinge verdien til
+		// å bli formatert med minst numericStringWidth bokstaver. Disse vil
+		// i denne koden bli satt til " " automatisk, istedenfor til 0, da det
+		// er lettere å lese mellomrom enn 0 for mennesker.
+		
+		// verdi-formatet vårt blir minst numericStringWidth langt
+		String fmt = "%" + numericStringWidth + "d";
+
+		StringBuilder sBuilder = new StringBuilder();		
+		for(int[] row : matrise) {
+			sBuilder.append("{ ");
+			int n = row.length;
+			for(int i = 0; i < n; i++) {
+				if(i > 0) {
+					sBuilder.append(',');
+				}
+				sBuilder.append(String.format(fmt, row[i]));
+			}
+			sBuilder.append(" }");
+			sBuilder.append('\n');
+		}
+		return sBuilder.toString();
+	}	
+	
+	// clone a 2d int matrix
+	public static int[][] cloneMatrix(int[][] matrise) {
+		int[][] result = new int[matrise.length][0];
+		int n = matrise.length;
+		for(int i=0; i<n; i++) {
+			result[i] = matrise[i].clone();
+		}
+		return result;	
+	}
+	
+	// make a 2d int matrix	of same dimension as input
+	public static int[][] cloneMatrixStructure(int[][] matrise) {
+		int[][] result = new int[matrise.length][0];
+		int n = matrise.length;
+		for(int i=0; i<n; i++) {
+			result[i] = new int[matrise[i].length];
+		}
+		return result;	
+	}
+	
+	// multiply two matrices, straight forward -> return (a x c)
+	// - matrices must be rectangular or quadratic
+	// - matrices must have non-zero dimensions 
+	// - a must have same number of columns that b has rows
+
+	public static int[][] multiply(int[][] a, int[][] b) {	
+		// should check dimensions ... 
+		// (3x3) X (3x4) is NOT ok
+		// (4x3) x (3x3) IS ok
+		
+		// lag tom kopi av matrise
+		int[][] result = MatrixUtils.cloneMatrixStructure(a);
+		
+		// get matrix dimensions, n = rows, m = cols
+		int numRows = a.length;		
+		int numCols = a[0].length;	
+		
+		int sumDotProduct;
+
+		// for each row
+		for(int r=0; r < numRows; r++) {		
+			// for each col, out[r][c] = (row a) dotProduct (column c) 
+			for(int c=0; c < numCols; c++) {				
+				sumDotProduct = 0;
+				
+				// compute dotproduct += (a[r][q] * b[q][c])
+				// similar to dotproduct = v1 * v2 
+				// where 
+				// 		v1 = (row vector from a)
+				// 		v2 = (column vector from b)
+				for(int q=0; q < numCols; q++) {
+					sumDotProduct += a[r][q] * b[q][c];
+				}
+
+				// result[r][c] = (rad r) dotProduct (kolonne c)
+				result[r][c] = sumDotProduct;
+			}
+		}
+		
+		return result;
+	}	
+	/*
+	// check if two 2d int matrices are identical
+	public static boolean equals(int[][] a, int[][] b) {		
+		// get outer array lengths
+		int n1 = a.length;
+		int n2 = b.length;
+		
+		// return false if lengths differ
+		if(n1 != n2) {
+			return false;
+		}
+		
+		for(int i=0; i<n1; i++) {
+			// get inner array lengths
+			int m1 = a[i].length;
+			int m2 = b[i].length;
+			
+			// return false if lengths differ
+			if(m1 != m2) {
+				return false;
+			}
+			
+			
+			// if(!Arrays.equals(a[i], b[i])){
+			// 		return false;
+			// }
+			for(int j=0; j<m1; j++) {
+				if(a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	*/	
+	
+	/*
+	// make a 2d int matrix	of same dimension as input
+	public static boolean equalMatrixStructure(int[][] a, int[][] b) {
+		int n1 = a.length;
+		int n2 = b.length;
+		
+		// return false if lengths differ
+		if(n1 != n2) { 
+			return false; 
+		}
+		
+		for(int i=0; i<n1; i++) {
+			int m1 = a[i].length;
+			int m2 = b[i].length;
+			
+			// return false if lengths differ			
+			if(m1 != m2) {
+				return false;
+			}
+		}
+		return true;		
+	}	
+	*/
+	
+	public static int getNumberOfColumns(int[][] a) {
+		return (a.length > 0) ? a[0].length : 0;
+	}
+	public static int getNumberOfRows(int[][] a) {
+		return a.length;
+	}
+};
+
+class VectorUtils{
+	public static String vectorToString(int a[]) {
+		return "[" + java.util.Arrays.toString(a) + "]";
+	}
+	
+	// ta dotproduct mellom to vektorer
+	public static int dotProduct(int[] a, int[] b) {
+		// krav at vektorer må ha lik lengde
+		if(a.length != b.length) {
+			throw new RuntimeException("vectors must have equal length");
+		}
+		
+		int sum = 0;
+		int n = a.length;
+		for(int i=0; i<n; i++) {
+			sum += (a[i] * b[i]);
+		}
+
+		return sum;
+	}	
+};
+
+public class Matriser {	
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		// bruk tilStreng metoden
+		String s = tilStreng(matrise);
+		System.out.println(s);
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		// alternative til StringBuilder er String s = ""; s += (uttrykk); 
+		StringBuilder sBuilder = new StringBuilder();
+		for(int[] row : matrise) {
+			for(int value : row) {
+				sBuilder.append(value);
+				sBuilder.append(' ');
+			}
+			sBuilder.append('\n');
+		}
+		return sBuilder.toString();
 	}
 
+
+			
+		
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
+		// allocate a new matrix for result variable
+		int[][] result = MatrixUtils.cloneMatrix(matrise);
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		int n = matrise.length;
+		for(int i=0; i<n; i++) {
+			int m = matrise[i].length;
+			for(int j=0; j<m; j++) {
+				result[i][j] *= tall;
+			}
+		}
+		
+		return result;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		// return MatrixUtils.equals(a, b);
 		
+		// get outer array lengths
+		int n1 = a.length;
+		int n2 = b.length;
+		
+		// return false if lengths differ
+		if(n1 != n2) {
+			return false;
+		}
+		
+		for(int i=0; i<n1; i++) {
+			// get inner array lengths
+			int m1 = a[i].length;
+			int m2 = b[i].length;
+			
+			// return false if lengths differ
+			if(m1 != m2) {
+				return false;
+			}
+			
+			for(int j=0; j<m1; j++) {
+				if(a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
-	// e)
+	// e) 
+	// oppgave: mirror matrix along diagonal from topleft to bottomrighjt
 	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+		int[][] result = MatrixUtils.cloneMatrixStructure(matrise);
+		
+		// fungerer bare for kvadratisk matrise så vi kan gjenbruke
+		// n i begge loops, en ikke kvadratisk matrise har ingen klart
+		// definert diagonal ved en-bort-en-ned tankegang
+		int n = matrise.length;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				result[i][j] = matrise[j][i];
+			}
+		}
+		return result;
 	}
 
 	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
+	// should check dimensions ... 
+	// (3x3) X (3x4) is NOT ok
+	// (4x3) x (3x3) IS ok	
+	// multiply two matrices, straight forward -> return (a x c)
+	// - matrices must be rectangular or quadratic
+	// - matrices must have non-zero dimensions 
+	// - a must have same number of columns that b has rows	
+	public static int[][] multipliser(int[][] a, int[][] b) {	
+		// lag tom kopi av matrise
+		int[][] result = MatrixUtils.cloneMatrixStructure(a);
+		
+		// get matrix dimensions, n = rows, m = cols
+		int numRows = a.length;		
+		int numCols = a[0].length;	
+		
+		int sumDotProduct;
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+		// for each row
+		for(int r=0; r < numRows; r++) {		
+			// for each col, out[r][c] = (row a) dotProduct (column c) 
+			for(int c=0; c < numCols; c++) {				
+				// reset accumulator and compute dot product
+				sumDotProduct = 0;
+				for(int q=0; q < numCols; q++) {
+					sumDotProduct += a[r][q] * b[q][c];
+				}
+
+				// result[r][c] = (row r) dotProduct (col c)
+				result[r][c] = sumDotProduct;
+			}
+		}
+		
+		return result;
 	}
 }
--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -4,60 +4,146 @@ public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
+		System.out.println("skrivUt(...)");
+		int n = tabell.length;
+		for(int i=0; i<n; i++) {
+			if(i > 0) {
+				System.out.print(",");
+			}
+			System.out.print(tabell[i]);
+		}
+		System.out.println("");
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
+		StringBuilder sBuilder = new StringBuilder();
+		
+		sBuilder.append("[");
+		int n = tabell.length;
+		for(int i=0; i<n; i++) {
+			if(i > 0) {
+				sBuilder.append(",");
+			}			
+			sBuilder.append(tabell[i]);
+		}
+		sBuilder.append("]");
+		
+		return sBuilder.toString();
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		
+		/*
+		// kode dersom en ikke vil bruke StringBuilder. Jeg lot parantes
+		// stå igjen rundt uttrykk sånn at en lett kan se at det var 
+		// kall til StringBuilder objektet der.		
+		String s = "";
+		
+		s += ("[");
+		int n = tabell.length;
+		for(int i=0; i<n; i++) {
+			if(i > 0) {
+				s += (",");
+			}			
+			s += (tabell[i]);
+		}
+		s += ("]");
+		
+		return s;
+		*/
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		// vi kunne sjekket om n var 0 og returnet tidlig, eller hvis n > 0: 
+		// satt sum til tabell[0] og loopet fra 1 istedenfor 0 men for 
+		// enkelhets skyld så kjører vi for-loop fra 0.
+		int n = tabell.length;
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			sum += tabell[i];
+		}
+		return sum;
 	}
 
 	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
+	public static boolean finnesTall(int[] tabell, int tall) {	
+		int n = tabell.length;
+		for(int i=0; i<n; i++) {
+			if(tabell[i] == tall) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int n = tabell.length;
+		for(int i=0; i<n; i++) {
+			if(tabell[i] == tall) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	// f)
-	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+	public static int[] reverser(int[] tabell) {	
+		int n = tabell.length;		
+		
+		// kopier tabell, vi kunne brukt tabell.clone();
+		int[] result = new int[n];
+		for(int i=0; i<n; i++) {
+			result[i] = tabell[i];
+		}
+		
+		// forslag Bjarte		
+		int halfLength = n / 2;	// int-division
+		for(int i=0; i<halfLength; i++) {
+			// swap [i] og [(n - 1) - i]
+			int tmp = result[i];
+			result[i] = result[n - 1 - i];
+			result[n - 1 - i] = tmp;
+		}
+		return result;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		// vi gjør en antagelse og tenker at stigende rekkefølge ikke betyr
+		// større enn men større eller lik da [1, 2, 2, 3] også bør detekteres
+		// som stigende. i en virkelig situasjon ville en sjekket hvilken som var
+		// korrekt for å unngå trøbbel med andre caller som kunne hatt forventning
+		// om at [1, 2, 2, 3] ikke var stigende. vi forstår at lærer kan ha ment strengt
+		// stigende men velger likevel >= slik at metoden detekterer korrekt om et array
+		// er sortert i (eller har) stigende rekkefølge. 
+		// 
+		// enhetstest vil ikke bry seg, vi sjekket med dataene
+		
+		int n = tabell.length;
+		for(int i=1; i<n; i++) {
+			if(!(tabell[i] >= tabell[i - 1])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
-
+		int n1 = tabell1.length;
+		int n2 = tabell2.length;
+		
+		int[] result = new int[n1 + n2];
+		int index = 0;
+		
+		for(int i=0; i<n1; i++) {
+			result[index++] = tabell1[i];
+		}
+		for(int i=0; i<n2; i++) {
+			result[index++] = tabell2[i];
+		}
+		
+		return result;
 	}
 }
